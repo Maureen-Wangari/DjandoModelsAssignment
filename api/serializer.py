a89ec4469c76b4ea4b4454e0782b46ce47133662
Maureen-Njeri <maureenwangarinjeri@gmail.com>
2024-07-18 07:04:58 +0300

    add school project models

---
+++ api/serializer.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from student.models import Student
+from teacher.models import Teacher
+from classes.models import Classes
+from courses.models import Courses
+from classPeriod.models import ClassPeriod
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = "__all__"
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= Teacher
+        fields='_all_'
+class  ClassesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Classes
+        fields='_all_'
+class CoursesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Courses
+        fields='_all_'
+class  ClassPeriodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=ClassPeriod
+        fields='_all_'
---
+++ student/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField()
+    id = models.AutoField(primary_key=True)
+    code = models.PositiveSmallIntegerField(unique=True)
+    gender = models.CharField(max_length=10)
+    country = models.CharField(max_length=100)
+    bio = models.TextField(blank=True, null=True)
+    enrollment_date = models.DateField()
+    guardian_phone_number = models.CharField(max_length=15)
+    guardian_name = models.CharField(max_length=100)
+    picture = models.ImageField()
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
